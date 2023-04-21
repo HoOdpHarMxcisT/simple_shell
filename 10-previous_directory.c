f56126e4a3d70b8609ed078fedb91708155a688b
Stanley <122971614+RSstanley@users.noreply.github.com>
2023-04-21 09:35:34 +0200

    Create 10-previous_directory.c

---
+++ 10-previous_directory.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 100 // maximum length of a command
+#define MAX_ARGUMENTS 10 // maximum number of arguments in a command
+extern char **environ; // environment variables
+
+void display_prompt() {
+    printf("simple_shell> "); // display the prompt
+}
+
+void read_command(char *command) {
+    if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
+        printf("\n"); // print new line after end of file (Ctrl+D)
+        exit(0); // exit gracefully on end of file
+    }
+}
+
+int execute_command(char *command, char **envp, char *current_directory) {
+    char *arguments[MAX_ARGUMENTS];
+    int num_arguments = 0;
+    
+    // tokenize the command into arguments
+    char *token = strtok(command, " \t\n");
+    while (token != NULL && num_arguments < MAX_ARGUMENTS - 1) {
+        arguments[num_arguments++] = token;
+        token = strtok(NULL, " \t\n");
+    }
+    arguments[num_arguments] = NULL; // set last argument to NULL for execvp
+    
+    // check if the command is the exit built-in
+    if (strcmp(arguments[0], "exit") == 0) {
+        exit(0); // exit gracefully
+    }
+    
+    // check if the command is the env built-in
+    if (strcmp(arguments[0], "env") == 0) {
+        char **env = envp;
+        while (*env != NULL) {
+            printf("%s\n", *env);
+            env++;
+        }
+        return 0;
+    }
+    
+    // check if the command is the cd built-in
+    if (strcmp(arguments[0], "cd") == 0) {
+        char *directory = arguments[1];
+        if (directory == NULL || strcmp(directory, "-") == 0) {
+            directory = getenv("HOME");
+        }
+        char previous_directory[MAX_COMMAND_LENGTH + 1];
+        if (getcwd(previous_directory, sizeof(previous_directory)) == NULL) {
+            perror("getcwd");
+            return 1;
+        }
+        if (chdir(directory) < 0) {
+            perror("chdir");
+            return 1;
+        }
+        if (setenv("PWD", directory, 1) < 0) {
+            perror("setenv");
+            return 1;
+        }
+        printf("%s\n", directory);
+        return 0;
+    }
+    
+    // check if the command exists in the PATH
+    char *path = getenv("PATH");
+    char *path_copy = strdup(path);
+    token = strtok(path_copy, ":");
+    while (token != NULL) {
+        char command_path[MAX_COMMAND_LENGTH + 1];
+        snprintf(command_path, sizeof(command_path), "%s/%s", token, arguments[0]);
+        if (access(command_path, X_OK) == 0) {
+            // fork a child process to execute the command
+            pid_t pid = fork();
+            if (pid < 0) {
+                perror("fork");
+                free(path_copy);
+                return 1;
+            } else if (pid == 0) {
+                // child process
+                if (execve(command_path, arguments, envp) < 0) {
+                    perror("execve");
+                    free(path_copy);
+                    exit(1);
+                }
+            } else {
+                // parent process
+                int status;
+                waitpid (pid, &status, 0);
+                free(path_copy);
+                return WIFEXITED(status) ? WEXITSTATUS(status) : 1;
+            }
+        }
+        token = strtok(NULL, ":");
+    }
+    free(path_copy);
+    
+    // command not found
+    printf("Command not found: %s\n", arguments[0]);
+    return 1;
+}
+
+int main() {
+    char command[MAX_COMMAND_LENGTH + 1];
+    char current_directory[MAX_COMMAND_LENGTH + 1];
+    int exit_status = 0;
+    
+    while (exit_status == 0) {
+        display_prompt();
+        read_command(command);
+        exit_status = execute_command(command, environ, current_directory);
+    }
+    
+    return exit_status;
+}
+
