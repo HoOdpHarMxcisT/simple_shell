05edc9d59392b2fcf1e5bc051e7706e1f4f8d5ea
RSstanley <stanleyrudzani@gmail.com>
2023-05-25 07:31:04 +0200

    old

--- shell.h
+++ shell.h
@@ -9,7 +9,6 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include <signal.h>
 
 int lsh_ctrld(char **args);
 int lsh_cd(char **args);
@@ -17,7 +16,7 @@ int lsh_help(char **args);
 extern char **environ;
 int lsh_exit(char **args);
 int _strcmp(char *s1, char *s2);
-size_t strncmp(char *s1, char *s2, size_t n);
+size_t _strncmp(char *s1, char *s2, size_t n);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
@@ -26,28 +25,10 @@ int _putchar(char c);
 char *_get_path(char **env);
 int _values_path(char **arg, char **env);
 char *_getline_command(void);
-
+void _getenv(char **env);
 char **tokenize(char *lineptr);
 void _exit_command(char **args, char *lineptr, int _exit);
 int _fork_fun(char **arg, char **av, char **env,
 char *lineptr, int np, int c);
-
-int stderr_pc(char c);
-int checkbi(char **tokens);
-int runbi(char **tokens);
-int executearg(char **arg);
-int count_words(char *str, char *delim);
-char *read_line(int n);
-char **parse_str(char *str, char *delim, int n);
-char *_strdup(char *s);
-char **_paths(void);
-char *_finder(char **paths, char *cmd);
-char *pathfinder(char **arg);
-char *pstart(char *allpaths);
-char *pend(char *allpaths);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void free_dptr(char **ptr);
-void badcom(int errorstatus, int count, char *cmd);
-void print_number(int n);
 
 #endif /* SHELL_H */
